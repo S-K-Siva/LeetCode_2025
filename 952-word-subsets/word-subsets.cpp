2e7a49447fcaa2c4ebe1aaebb303578ff42200dd
Siva Sakthivel <88235149+S-K-Siva@users.noreply.github.com>
2025-01-10 16:31:25 +0530

    Time: 56 ms (59.82%) | Memory: 106.8 MB (42.47%) - LeetSync

---
+++ 952-word-subsets/word-subsets.cpp
@@ -0,0 +1,76 @@
+// class Solution {
+// public:
+//     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+//         vector<string> result;
+//         unordered_map<string, map<char,int>> track;
+//         for(auto word : words1){
+
+//             map<char,int> freq;
+//             for(int i = 0;i<word.size();i++){
+//                 ++freq[word[i]];
+//             }
+//             track[word] = freq;
+//         }
+//         map<string,map<char,int>> letterFreq;
+//         for(auto word : words2){
+//             map<char,int> freq;
+//             for(int i = 0;i<word.size();i++){
+//                 ++freq[word[i]];
+//             }
+//             letterFreq[word] = freq;
+//         }
+//         for(auto word : words1){
+//             bool possible = true;
+//             map<char,int> freq = track[word];
+//             for(auto letter : words2){
+//                 map<char,int> lf = letterFreq[letter];
+//                 for(auto it : lf){
+//                     if(it.second > freq[it.first]){
+//                         possible = false;
+//                         break;
+//                     }
+//                 }
+//             }
+//             if(possible){
+//                 result.push_back(word);
+//             }
+//         }
+//         return result;
+//     }
+// };
+
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<string> result;
+        vector<int> maxFreq(26, 0);
+        for (auto word : words2) {
+            vector<int> freq(26, 0);
+            for (char ch : word) {
+                freq[ch - 'a']++;
+            }
+            for (int i = 0; i < 26; ++i) {
+                maxFreq[i] = max(maxFreq[i], freq[i]);
+            }
+        }
+
+        for (auto word : words1) {
+            vector<int> freq(26, 0);
+            for (char ch : word) {
+                freq[ch - 'a']++;
+            }
+            bool isPossible = true;
+            for (int i = 0; i < 26; ++i) {
+                if (freq[i] < maxFreq[i]) {
+                    isPossible = false;
+                    break;
+                }
+            }
+            if (isPossible) {
+                result.push_back(word);
+            }
+        }
+
+        return result;
+    }
+};
