a0875375d03732cde8ff1ddbf6074c1332d6abb8
Siva Sakthivel <88235149+S-K-Siva@users.noreply.github.com>
2025-01-27 11:34:20 +0530

    Time: 39 ms (76.13%) | Memory: 14.5 MB (46.72%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -14,31 +14,49 @@ public:
     // }
 
     //memo
-    int solve(vector<int>& nums, int index,int prevIndex,
-    vector<vector<int>>& dp){
-        if(index >= nums.size()){
-            return 0;
-        }
-        if(dp[index][prevIndex + 1] != -1){
-            return dp[index][prevIndex + 1];
-        }
-        int length = solve(nums,index+1,prevIndex,dp);
-        if(prevIndex == -1 || nums[index] > nums[prevIndex]){
-            length = max(length,1 + solve(nums,index+1,index,dp));
-        }
-        return dp[index][prevIndex + 1] = length;
-    }
+    // int solve(vector<int>& nums, int index,int prevIndex,
+    // vector<vector<int>>& dp){
+    //     if(index >= nums.size()){
+    //         return 0;
+    //     }
+    //     if(dp[index][prevIndex + 1] != -1){
+    //         return dp[index][prevIndex + 1];
+    //     }
+    //     int length = solve(nums,index+1,prevIndex,dp);
+    //     if(prevIndex == -1 || nums[index] > nums[prevIndex]){
+    //         length = max(length,1 + solve(nums,index+1,index,dp));
+    //     }
+    //     return dp[index][prevIndex + 1] = length;
+    // }
     int lengthOfLIS(vector<int>& nums) {
-        int maxi = INT_MIN;
-        int cnt = 0;
-        int prev = -1;
         int n = nums.size();
-        // recursion
+
+
+        
+        // // recursion
+        // int maxi = INT_MIN;
+        // int cnt = 0;
+        // int prev = INT_MIN;
         // solve(nums,0,cnt,maxi,prev);
         // return maxi == INT_MIN ? -1 : maxi;
 
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        // memo
-        return solve(nums,0,prev,dp);
+        // // memo
+        // int prev = -1;
+        // vector<vector<int>> dp(n,vector<int>(n+1,-1));
+        
+        // return solve(nums,0,prev,dp);
+
+        // bottom-up
+        vector<int> dp(n,1);
+        int res = INT_MIN;
+        for(int i = 1;i<n;i++){
+            for(int j = 0;j<i;j++){
+                if(nums[i] > nums[j]){
+                    dp[i] = max(dp[i],dp[j] + 1);
+                    res = max(res, dp[i]);
+                }
+            }
+        }
+        return res == INT_MIN ?  1 : res;
     }
 };
