ad652349a886ac021792b12c6446155a3a270645
Siva Sakthivel <88235149+S-K-Siva@users.noreply.github.com>
2025-02-08 00:30:01 +0530

    Time: 99 ms (95.82%) | Memory: 157.7 MB (49.84%) - LeetSync

---
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> queryResults(int limit, vector<vector<int>>& queries) {
+        unordered_map<int, int> trackIndex; 
+        unordered_map<int, int> trackColour; 
+        vector<int> result;
+
+        for (auto& row : queries) {
+            int ball = row[0];
+            int color = row[1];
+
+            if (trackIndex.find(ball) != trackIndex.end()) {
+                int previousColor = trackIndex[ball];
+                
+                if (previousColor == color) {
+                    result.push_back(trackColour.size());
+                    continue;
+                }
+
+                if (--trackColour[previousColor] == 0) {
+                    trackColour.erase(previousColor);
+                }
+            }
+
+            trackIndex[ball] = color;
+            trackColour[color]++;
+
+            result.push_back(trackColour.size());
+        }
+
+        return result;
+    }
+};
