512a95b65957dd35d689d58d6fe2b5af3c75d9d8
Siva Sakthivel <88235149+S-K-Siva@users.noreply.github.com>
2025-02-26 22:18:38 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (6.06%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
-        int maxEndingHere = 0, minEndingHere = 0;
-        int maxSum = INT_MIN, minSum = INT_MAX;
+        int n = nums.size();
+        vector<int> prefixSum(n, 0);
+        prefixSum[0] = nums[0];
 
-        for (int num : nums) {
-            maxEndingHere = max(num, maxEndingHere + num);
-            maxSum = max(maxSum, maxEndingHere);
-
-            minEndingHere = min(num, minEndingHere + num);
-            minSum = min(minSum, minEndingHere);
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
         }
 
-        return max(maxSum, abs(minSum));
+        int maxPrefix = 0, minPrefix = 0;
+        int result = INT_MIN;
+
+        for (int i = 0; i < n; i++) {
+            result = max(result, abs(prefixSum[i] - minPrefix));
+            result = max(result, abs(prefixSum[i] - maxPrefix));
+
+            maxPrefix = max(maxPrefix, prefixSum[i]);
+            minPrefix = min(minPrefix, prefixSum[i]);
+        }
+
+        return result;
     }
 };
