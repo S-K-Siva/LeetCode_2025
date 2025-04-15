e1712e0932317f785c1e0c9011e4ca83f310989b
Siva Sakthivel <88235149+S-K-Siva@users.noreply.github.com>
2025-04-15 12:16:36 +0530

    Time: 343 ms (75.01%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-more-than-5-students/classes-more-than-5-students.sql
+++ 596-classes-more-than-5-students/classes-more-than-5-students.sql
@@ -1,3 +1,6 @@
 # Write your MySQL query statement below
-select distinct class from courses c1
-where (select count(*) as cnt from courses c2 where c2.class = c1.class) >= 5;
+-- select distinct class from courses c1
+-- where (select count(*) as cnt from courses c2 where c2.class = c1.class) >= 5;
+
+select class from courses
+group by class having count(class) >= 5;
