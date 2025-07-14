3ecac3d794f846e221519b81492b033995ec6e6d
Siva Sakthivel <88235149+S-K-Siva@users.noreply.github.com>
2025-07-14 22:44:28 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (39.86%) - LeetSync

--- 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -9,29 +9,40 @@
  * };
  */
 class Solution {
+private:
+    int exp = 0;
 public:
+    
     int getDecimalValue(ListNode* head) {
-        ListNode *result;
-        result = new ListNode(-1);
+        if(head == nullptr) return 0;
+        int value = getDecimalValue(head->next);
+        if(head->val == 1){
+            value += pow(2,exp);
+        }
+        exp++;
+        return value;
+        //without recursion
+        // ListNode *result;
+        // result = new ListNode(-1);
 
-        while(head != nullptr){
-            ListNode *newNode;
-            newNode = new ListNode(head->val);
-            newNode->next = result;
-            result = newNode;
-            head = head->next;
-        }
+        // while(head != nullptr){
+        //     ListNode *newNode;
+        //     newNode = new ListNode(head->val);
+        //     newNode->next = result;
+        //     result = newNode;
+        //     head = head->next;
+        // }
         
-        int exp = 0;
-        int res = 0;
-        while(result != nullptr){
-            if(result->val == -1) break;
-            if(result->val == 1){
-                res += pow(2,exp);
-            }
-            exp++;
-            result = result->next;
-        }
-        return res;
+        // int exp = 0;
+        // int res = 0;
+        // while(result != nullptr){
+        //     if(result->val == -1) break;
+        //     if(result->val == 1){
+        //         res += pow(2,exp);
+        //     }
+        //     exp++;
+        //     result = result->next;
+        // }
+        // return res;
     }
 };
