5d0296ad26b7e77af35bfbddd98219c644bd3b78
Siva Sakthivel <88235149+S-K-Siva@users.noreply.github.com>
2025-09-15 23:27:44 +0530

    Time: 63 ms (22.20%) | Memory: 21.5 MB (26.79%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -16,8 +16,14 @@ public:
     }
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n,-1);
+        vector<int> dp(n,1e9);
         dp[n-1] = 0;
-        return helper(0,n,nums,dp);
+        for(int i = n-2;i>=0;i--){
+            for(int j = 1;j<=nums[i] && j+i < n;j++){
+                dp[i] = min(dp[i],1 + dp[i+j]);
+            }
+        }
+        return dp[0];
+        // return helper(0,n,nums,dp);
     }
 };
